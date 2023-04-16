0189a81720ae1ac9cc2605204aed0f64b62b7018
Masha Vilenchyk <mariavilenchyk@gmail.com>
2023-04-16 15:32:10 +0300

    feat: towel(April 16, 2023 15.31)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let res = [];
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            let columnInd =
+                i % 2 === 0
+                    ? //  если 0 - значит ряд четный,
+                      j
+                    : // а если нет, то нечетный
+                      matrix[i].length - j - 1;
+            res += matrix[i][columnInd];
+        }
+    }
+    return Array.from(res);
+};
